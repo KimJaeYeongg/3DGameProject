871ab96b0dff7f8cdef2541013bc8a74005c08dc
rootinge <jhpark5129@gmail.com>
2022-11-19 20:33:51 +0900

    player 카메라 이동
    카메라 스무스하게 이동

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -11,6 +11,7 @@ public class FollowCam : MonoBehaviour
     public float distance = 5.0f;
     public float dampTrace = 0.8f;
     public float camAngle = 270;
+    public float camAngleSpeed = 5.0f;
 
     public int turnBox = 0;
 
@@ -29,57 +30,117 @@ public class FollowCam : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        CamTurn();
+      
         // 카메라 x y z 좌표값
         tr.position = Vector3.Lerp(tr.position, targetTr.position + (Vector3.right * x_distance)
             + (Vector3.up * height) + (Vector3.forward * z_distance), dampTrace);
+    }
+
+    void FixedUpdate()
+    {
+        CamTurn();
     }
 
     void CamTurn()
     {
         if(turnBox == 0)
         {
-            x_distance = distance;
-            z_distance = 0;
+            if (camAngle == 0)
+                camAngle = 360;
+
+            //x_distance = distance;
+            //z_distance = 0;
 
             if (camAngle < 270)
-                camAngle++;
+            {
+                camAngle += camAngleSpeed;
+                if (x_distance < distance)
+                    x_distance += distance / 90 * camAngleSpeed;
+                if (z_distance > 0)
+                    z_distance -= distance / 90 * camAngleSpeed;
+            }
             else if (camAngle > 270)
-                camAngle--;
-
-            tr.rotation = Quaternion.Euler(0, camAngle, 0); 
+            {
+                camAngle -= camAngleSpeed;
+                if (x_distance < distance)
+                    x_distance += distance / 90 * camAngleSpeed;
+                if (z_distance < 0)
+                    z_distance += distance / 90 * camAngleSpeed;
+            }
         }
         else if (turnBox == 1)
         {
-            x_distance = 0;
-            z_distance = distance;
+            //x_distance = 0;
+            //z_distance = distance;
 
             if (camAngle < 180)
-                camAngle++;
+            {
+                camAngle += camAngleSpeed;
+                if (z_distance < distance)
+                    z_distance += distance / 90 * camAngleSpeed;
+                if (x_distance < 0)
+                    x_distance += distance / 90 * camAngleSpeed;
+            }
             else if (camAngle > 180)
-                camAngle--;
-            tr.rotation = Quaternion.Euler(0, camAngle, 0);
+            {
+                camAngle -= camAngleSpeed;
+                if (z_distance < distance)
+                    z_distance += distance / 90 * camAngleSpeed;
+                if (x_distance > 0)
+                    x_distance -= distance / 90 * camAngleSpeed;
+            }
         }
         else if (turnBox == 2)
         {
-            x_distance = -distance;
-            z_distance = 0;
+            //x_distance = -distance;
+            //z_distance = 0;
 
             if (camAngle < 90)
-                camAngle++;
+            {
+                camAngle += camAngleSpeed;
+
+                if (x_distance > -distance)
+                    x_distance -= distance / 90 * camAngleSpeed;
+                if (z_distance < 0)
+                    z_distance += distance / 90 * camAngleSpeed;
+            }
             else if (camAngle > 90)
-                camAngle--;
-            tr.rotation = Quaternion.Euler(0, camAngle, 0);
+            {
+                camAngle -= camAngleSpeed;
+                if (x_distance > -distance)
+                    x_distance -= distance / 90 * camAngleSpeed;
+                if (z_distance > 0)
+                    z_distance -= distance / 90 * camAngleSpeed;
+            }
         }
         else if (turnBox == 3)
         {
-            x_distance = 0;
-            z_distance = -distance;
+            if (camAngle == 270)
+                camAngle = -90;
+            //x_distance = 0;
+            //z_distance = -distance;
+
             if (camAngle < 0)
-                camAngle++;
+            {
+                camAngle += camAngleSpeed;
+
+                if (z_distance > -distance)
+                    z_distance -= distance / 90 * camAngleSpeed;
+                if(x_distance > 0)
+                    x_distance -= distance / 90 * camAngleSpeed; 
+            }
             else if (camAngle > 0)
-                camAngle--;
-            tr.rotation = Quaternion.Euler(0, camAngle, 0);
+            {
+                
+                camAngle -= camAngleSpeed;
+                if (z_distance > -distance)
+                    z_distance -= distance / 90 * camAngleSpeed;
+                if (x_distance < 0)
+                    x_distance += distance / 90 * camAngleSpeed;
+            }
         }
+
+        
+            tr.rotation = Quaternion.Euler(0, camAngle, 0);
     }
 }
